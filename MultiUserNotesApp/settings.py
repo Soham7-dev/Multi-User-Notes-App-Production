a82737fc786a32169a7856c7fbe4b3d7a0730cec
Soham Das <Soham.Das@clinisys.com>
2023-02-20 18:47:18 +0530

    Some changes

--- MultiUserNotesApp/settings.py
+++ MultiUserNotesApp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v4fb%)xfx02%d!52ck!fs!7ye00tn!c!mrxzb!5bhvvz)otzvf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
